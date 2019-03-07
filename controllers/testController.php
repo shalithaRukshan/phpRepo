9e975ab04ce049619c7671c811707338c9c0d81b
shalitha <mdsrwijethilaka@gmail.com>
2019-03-07 16:08:46 +0530

    Update testController.php

--- controllers/testController.php
+++ controllers/testController.php
@@ -10,3 +10,13 @@ echo 'This is a stssss file';
 echo 'This is ss file';
 echo 'This is a stssss file';
 echo 1/0;
+
+class Clazz {
+  $name=NULL;  // instance variable
+
+  public static function foo(){
+    if ($this->name != NULL) {
+      // ...
+    }
+  }
+}
